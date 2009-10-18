ea7efbc2bc65ba53b464d648421f0bb416f17713
Jin Lu <admin@niubi.de>
2009-10-18 15:08:01 +0000

    remove dupricated lib

--- utils/http/models.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from google.appengine.ext import db
-
-class RequestLogger(db.Model):
-    request = db.TextProperty()
-    response = db.TextProperty()
-    create_time = db.DateTimeProperty(auto_now_add=True)
